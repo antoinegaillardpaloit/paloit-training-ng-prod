2eb1b855cf3f885b2373dc50ce77f877a5ce12ca
Antoine Gaillard <agaillard@palo-it.com>
2020-09-09 19:35:30 +0200

    formateurs page almost done

--- src/app/components/formateur/formateur.component.ts
+++ src/app/components/formateur/formateur.component.ts
@@ -10,6 +10,9 @@ import { environment } from 'src/environments/environment';
 export class FormateurComponent implements OnInit {
 
   @Input() formateur: any;
+  @Input() orientationIndex: number;
+  orientationClass: string = '';
+
   apiUrl: string = environment.apiUrl;
   photoUrl: string;
 
@@ -17,6 +20,7 @@ export class FormateurComponent implements OnInit {
 
   ngOnInit(): void {
     this.photoUrl = this.apiUrl + this.formateur.photo.url;
+    this.orientationClass = this.orientationIndex%2 === 0 ? 'palo-formateur-left' : 'palo-formateur-right';
   }
 
 }
